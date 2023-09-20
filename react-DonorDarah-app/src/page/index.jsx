4b075d6de5127dbab7c60f17b388a94112ba742f
abdisavia <ridhodanabdi7a@gmail.com>
2023-09-20 21:36:18 +0700

    Upload File

---
+++ react-DonorDarah-app/src/page/index.jsx
@@ -0,0 +1,16 @@
+import Navigasi from "../components/navbar";
+import "../css/landingPage.css";
+
+
+export default function LandingPage(){
+    return(
+        <>
+            <Navigasi />
+            <section id="heroSection">
+                <div>
+                    <h1></h1>
+                </div>
+            </section>
+        </>
+    )
+}
