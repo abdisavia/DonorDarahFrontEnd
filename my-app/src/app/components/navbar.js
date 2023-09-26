d5b44ad3da0cdfd66bf31936afdad21e7c4f8d54
jepan <jheffanD.com>
2023-09-26 22:37:58 +0700

    modified:   my-app/src/app/components/navbar.js 	modified:   my-app/src/app/pages/Login/page.js 	new file:   my-app/src/app/styles/css/login.css

--- my-app/src/app/components/navbar.js
+++ my-app/src/app/components/navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props){
                     <a href="/pages/kebutuhan" className="px-[15px] py-[15px] relative after:absolute hover:after:border-b-2 after:border-red after:w-full after:h-full after:left-0 after:bottom-0">Kebutuhan</a>
                     <a href="" className="px-[15px] py-[15px] relative after:absolute hover:after:border-b-2 after:border-red after:w-full after:h-full after:left-0 after:bottom-0">Forum</a>
                     <a href="" className="px-[15px] py-[15px] relative after:absolute hover:after:border-b-2 after:border-red after:w-full after:h-full after:left-0 after:bottom-0">Tentang</a>
-                    <a href=""  className="px-[20px] py-2 bg-red text-white font-bold rounded-full">Login</a>
+                    <a href="/pages/Login"  className="px-[20px] py-2 bg-red text-white font-bold rounded-full">Login</a>
                 </div>
             </div>
         </nav>
---
+++ my-app/src/app/pages/Login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles/css/login.css";
+
+
+function masuk() {
+    return (
+      <div className="my-component" >Ini adalah komponen saya</div>
+    );
+  }
+  
+  export default masuk;
