0411cc1e4c02c6d8f6b69beba834e06671d2b734
jepan <jheffanD.com>
2023-10-04 20:20:40 +0700

    new file:   my-app/src/app/components/register/dropdown_kabupaten.jsx 	new file:   my-app/src/app/components/register/dropdown_kelurahan.jsx 	new file:   my-app/src/app/components/register/dropdown_provinsi.jsx 	new file:   my-app/src/app/components/register/dropdwon_kecamatan.jsx 	modified:   my-app/src/app/pages/OTP/page.jsx 	modified:   my-app/src/app/pages/register/page.jsx

--- my-app/src/app/pages/register/page.jsx
+++ my-app/src/app/pages/register/page.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar";
 import "../../styles/css/regis.css";
+import "../../styles/css/login.css";
 
 export default function register() {
   return (
@@ -7,8 +8,29 @@ export default function register() {
       <div className="my-bg">
         <Navbar itemsColor="text-white" />
         <div className="row">
-          <div className="rectengel">
+          <div class="rectangle-36">
+            <div className="wraper text-center">
+              <label className="text-black font-Title text-[40px]">Register</label>
+              <form>
+                <div className="input-container">
+                  <img
+                    className="input-icon"
+                    src="/img/phone.svg"
+                    alt="Icon"
+                    height={20}
+                    width={20}
+                  />
+                  <input
+                    className="border-2 border-black rounded w-[280px] h-[40px] px-10"
+                    type="tel"
+                    placeholder="Masukkan Nomer Anda"
+                    name="noTelp"
+                  />
+                </div>
+                <p className="tulisan text-xs"></p>
 
+              </form>
+            </div>
           </div>
         </div>
       </div>
