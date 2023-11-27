9f703bb02424dc8920b1922c6159781589fb2526
jepan <jheffanD.com>
2023-11-27 21:09:39 +0700

    modified:   my-app/src/_components/navbar.js 	new file:   my-app/src/app/(pages)/forum/forum.jsx

---
+++ my-app/src/app/(pages)/forum/forum.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/_components/navbar";
+
+export default function Forum() {
+  return (
+    <>
+      <div>
+        <Navbar itemsColor="text-white" />
+        <h1>hallo</h1>
+      </div>
+    </>
+  );
+}
